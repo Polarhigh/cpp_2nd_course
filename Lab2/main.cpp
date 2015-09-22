92a5cf2ed89d6905887495324509fbf3433974b5
Polarhigh <trofian@yahoo.com>
2015-09-22 23:50:24 +0300

    2 and 6 lab upd

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -30,106 +30,127 @@
 using namespace std;
 using namespace lex;
 
-//const int UTF8_MAGIC = 0x10ffff;
+const int UTF8_MAGIC = 0x10ffff;
 
 enum class Lexeme
 {
-	WORD,
-	PUNCT_MARK,
-	SPACE,
-	TABULATION,
-	NEWLINE,
-	UNKNOWN
+    WORD,
+    PUNCT_MARK,
+    SPACE,
+    TABULATION,
+    NEWLINE,
+    UNKNOWN
+};
+
+// специальный функтор для transform, позволяет выводить не более 40 символов на строку без разрыва слов
+struct NewlineFunct : unary_function<token_t<Lexeme>, wstring>
+{
+    int curLen = 0;
+
+    wstring operator()(const token_t<Lexeme>& tok)
+    {
+        if (tok.value.length() + curLen >= 40)
+        {
+            curLen = tok.value.length();
+            return L"\n" + tok.value;
+        }
+        else
+        {
+            curLen += tok.value.length();
+            return tok.value;
+        }
+    }
 };
 
 void ApplyRules(vector<token_t<Lexeme>>& tokens)
 {
-	// ## требуется 1 токен
-	// замена табуляции и пероносов строк
-	replace_if(tokens.begin(), tokens.end(), [](const token_t<Lexeme>& tok) { return tok.lexeme == Lexeme::NEWLINE || tok.lexeme == Lexeme::TABULATION; }, token_t<Lexeme>(Lexeme::SPACE, L" "));
+    // ## требуется 1 токен
+    // замена табуляции и пероносов строк
+    replace_if(tokens.begin(), tokens.end(), [](const token_t<Lexeme>& tok) { return tok.lexeme == Lexeme::NEWLINE || tok.lexeme == Lexeme::TABULATION; }, token_t<Lexeme>(Lexeme::SPACE, L" "));
 
-	//замена слова длинной более 10 символов на Vau!!!
-	replace_if(tokens.begin(), tokens.end(), [](const token_t<Lexeme>& tok) { return tok.lexeme == Lexeme::WORD && tok.value.length() > 10; }, token_t<Lexeme>(Lexeme::WORD, L"Vau!!!"));
+    //замена слова длинной более 10 символов на Vau!!!
+    replace_if(tokens.begin(), tokens.end(), [](const token_t<Lexeme>& tok) { return tok.lexeme == Lexeme::WORD && tok.value.length() > 10; }, token_t<Lexeme>(Lexeme::WORD, L"Vau!!!"));
 
-	// ## требуется 2 токена
-	if (tokens.size() > 1)
-	{
-		// удаление нескольких пробелов идущих подряд
-		tokens.erase(unique(tokens.begin(), tokens.end(), [](const token_t<Lexeme>& lhs, const token_t<Lexeme>& rhs) { return lhs.lexeme == Lexeme::SPACE && lhs.lexeme == rhs.lexeme; }), tokens.end());
+    // ## требуется 2 токена
+    if (tokens.size() > 1)
+    {
+        // удаление нескольких пробелов идущих подряд
+        tokens.erase(unique(tokens.begin(), tokens.end(), [](const token_t<Lexeme>& lhs, const token_t<Lexeme>& rhs) { return lhs.lexeme == Lexeme::SPACE && lhs.lexeme == rhs.lexeme; }), tokens.end());
 
-		//добавление пробела после знака препинания
-		Lexeme prevLex, curLex, nextLex;
-		for (auto it = tokens.begin(); it != tokens.end() - 1; ++it)
-		{
-			curLex = (*it).lexeme;
-			nextLex = (*(it + 1)).lexeme;
+        //добавление пробела после знака препинания
+        Lexeme prevLex, curLex, nextLex;
+        for (auto it = tokens.begin(); it != tokens.end() - 1; ++it)
+        {
+            curLex = (*it).lexeme;
+            nextLex = (*(it + 1)).lexeme;
 
-			if (curLex == Lexeme::PUNCT_MARK && nextLex != Lexeme::SPACE)
-				it = tokens.insert(it + 1, token_t<Lexeme>(Lexeme::SPACE, L" "));
-		}
+            if (curLex == Lexeme::PUNCT_MARK && nextLex != Lexeme::SPACE)
+                it = tokens.insert(it + 1, token_t<Lexeme>(Lexeme::SPACE, L" "));
+        }
 
-		// ## требуется 3 токена
-		if (tokens.size() > 2)
-		{
-			// удаление пробела между словом и знаком препинания
-			for (auto it = tokens.begin() + 1; it != tokens.end() - 1;)
-			{
-				prevLex = (*(it - 1)).lexeme;
-				curLex = (*it).lexeme;
-				nextLex = (*(it + 1)).lexeme;
+        // ## требуется 3 токена
+        if (tokens.size() > 2)
+        {
+            // удаление пробела между словом и знаком препинания
+            for (auto it = tokens.begin() + 1; it != tokens.end() - 1;)
+            {
+                prevLex = (*(it - 1)).lexeme;
+                curLex = (*it).lexeme;
+                nextLex = (*(it + 1)).lexeme;
 
-				if (prevLex == Lexeme::WORD && curLex == Lexeme::SPACE && nextLex == Lexeme::PUNCT_MARK)
-					it = tokens.erase(it);
-				else
-					++it;
-			}
-		}
-	}
+                if (prevLex == Lexeme::WORD && curLex == Lexeme::SPACE && nextLex == Lexeme::PUNCT_MARK)
+                    it = tokens.erase(it);
+                else
+                    ++it;
+            }
+        }
+    }
 }
 
 void FileToString(const wchar_t* file, wstring& outText)
 {
-	wifstream wif;
-	wstringstream wss;
+    wifstream wif;
+    wstringstream wss;
 
-	wif.open(file, ios_base::in);
-	if (wif.is_open())
-	{
-		//wif.imbue(locale(wif.getloc(), new codecvt_utf8<wchar_t, UTF8_MAGIC, consume_header>()));
-		wss << wif.rdbuf();
-		outText = wss.str();
-	}
+    wif.open(file, ios_base::in);
+    if (wif.is_open())
+    {
+        wif.imbue(locale(wif.getloc(), new codecvt_utf8<wchar_t, UTF8_MAGIC, consume_header>()));
+        wss << wif.rdbuf();
+        outText = wss.str();
+    }
 }
 
 int main()
 {
-	locale::global(locale(""));
+    locale::global(locale(""));
 
-	wstring text;
-	vector<token_t<Lexeme>> tokens;
-	map<Lexeme, wregex> lexRules =
-	{
-		{ Lexeme::WORD,			wregex(L"[a-zA-Zа-яА-Я]+") },
-		{ Lexeme::PUNCT_MARK,	wregex(L"[\\.,!\\?:;]") },
-		{ Lexeme::SPACE,		wregex(L" ") },
-		{ Lexeme::TABULATION,	wregex(L"\\t") },
-		{ Lexeme::NEWLINE,		wregex(L"\\n") },
-	};
+    wstring text;
+    vector<token_t<Lexeme>> tokens;
+    map<Lexeme, wregex> lexRules =
+    {
+        { Lexeme::WORD,			wregex(L"[a-zA-Zа-яА-Я]+") },
+        { Lexeme::PUNCT_MARK,	wregex(L"[\\.,!\\?:;]") },
+        { Lexeme::SPACE,		wregex(L" ") },
+        { Lexeme::TABULATION,	wregex(L"\\t") },
+        { Lexeme::NEWLINE,		wregex(L"\\n") },
+    };
 
-	FileToString(L"C:\\Users\\PolarHIGH\\Documents\\On Line\\lab2_oop.txt", text);
+    FileToString(L"main.cpp", text);
 
-	wcout << L"[Исходный текст]" << endl;
-	wcout << text << endl << endl << endl;
+    wcout << L"[Исходный текст]" << endl;
+    wcout << text << endl << endl << endl;
 
-	LexParse(text, lexRules, tokens);
-	ApplyRules(tokens);
-	TokensToString(tokens, text);
+    LexParse(text, lexRules, tokens); // в Debug сборке время парсинга > 1с
+    ApplyRules(tokens);
+    TokensToString(tokens, text);
 
-	wcout << L"[Преобразованный текст]" << endl;
+    wcout << L"[Преобразованный текст]" << endl;
+    vector<wstring> stext;
+    transform(tokens.begin(), tokens.end(), back_inserter(stext), NewlineFunct());
+    
+    copy(stext.begin(), stext.end(), ostream_iterator<wstring, wchar_t>(wcout, L""));
+    wcout << endl;
 
-	// @TODO вывод по 40 символов на строку
-
-	wcout << text << endl << endl;
-
-	return 0;
+    return 0;
 }
--- Lab6/main.cpp
+++ Lab6/main.cpp
@@ -8,14 +8,14 @@ c. Вывести список слов присутствующий в текс
 
 
 2. Написать программу, которая выполняет следующие действия (все операции должны выполняться с помощью стандартных алгоритмов):
-a. Заполняет вектор геометрическими фигурами. Геометрическая фигура может быть треугольником, квадратом, прямоугольником или пяти угольником.
-b. Подсчитывает общее количество вершин всех фигур содержащихся в векторе (так треугольник добавляет к общему числу 3, квадрат 4 и т.д.)
-c. Подсчитывает количество треугольников, квадратов и прямоугольников в векторе
-d. Удаляет все пятиугольники
-e. На основании этого вектора создает vector<Point>, который содержит координаты одной из вершин (любой) каждой фигуры, т.е.
-   первый элемент этого вектора содержит координаты одной из вершину первой фигуры, второй элемент этого вектора содержит координаты одной из вершину второй фигуры и т.д.
-f. Изменяет вектор так, чтобы он содержал в начале все треугольники, потом все квадраты, а потом прямоугольники.
-g. Распечатывает вектор после каждого этапа работы
+    a. Заполняет вектор геометрическими фигурами. Геометрическая фигура может быть треугольником, квадратом, прямоугольником или пяти угольником.
+    b. Подсчитывает общее количество вершин всех фигур содержащихся в векторе (так треугольник добавляет к общему числу 3, квадрат 4 и т.д.)
+    c. Подсчитывает количество треугольников, квадратов и прямоугольников в векторе
+    d. Удаляет все пятиугольники
+    e. На основании этого вектора создает vector<Point>, который содержит координаты одной из вершин (любой) каждой фигуры, т.е.
+       первый элемент этого вектора содержит координаты одной из вершину первой фигуры, второй элемент этого вектора содержит координаты одной из вершину второй фигуры и т.д.
+    f. Изменяет вектор так, чтобы он содержал в начале все треугольники, потом все квадраты, а потом прямоугольники.
+    g. Распечатывает вектор после каждого этапа работы
 */
 
 #include <sstream>
@@ -25,6 +25,7 @@ g. Распечатывает вектор после каждого этапа 
 #include <algorithm>
 #include <numeric>
 #include <vector>
+#include "../Lab1/container_utils.h"
 
 using namespace std;
 
@@ -42,6 +43,12 @@ struct Point
 	}
 };
 
+ostream& operator<<(ostream& os, const Point& point)
+{
+    os << "(" << point.x << ", " << point.y << ")";
+    return os;
+}
+
 struct Shape
 {
 	vector<Point> vertexes;
@@ -56,6 +63,13 @@ struct Shape
 		return static_cast<int>(vertexes.size());
 	}
 };
+
+ostream& operator<<(ostream& os, const Shape& shape)
+{
+    os << "[Shape] vertexes: " << shape.GetVertexNum() << "\tpoints: ";
+    PrintContainer(shape.vertexes, " ");
+    return os;
+}
 
 struct IsSquareFunct : public unary_function<Shape, bool>
 {
@@ -105,28 +119,48 @@ void ReadFile(const char* file, string& outString)
 void Task2()
 {
 	vector<Shape> shapes;
-	// a) TODO заполнение вектора рандомом
+	// a) TODO заполнение вектора рандомом, UPD1: можно и без рандома обойтись
 	shapes.emplace_back(vector<Point>({ Point(0, 0), Point(0, 4), Point(6, 4), Point(6, 0) }), true);
-	shapes.emplace_back(vector<Point>({ Point(0, 0), Point(0, 4), Point(4, 4), Point(4, 0) }), true);
 	shapes.emplace_back(vector<Point>({ Point(0, 0), Point(0, 4), Point(4, 6), Point(0, 6), Point(12, -12) }));
+    shapes.emplace_back(vector<Point>({ Point(1, -40), Point(3, 4), Point(4, 0) }));
+    shapes.emplace_back(vector<Point>({ Point(0, 0), Point(0, 5), Point(5, 5), Point(5, 0) }), true); // квадрат
+    shapes.emplace_back(vector<Point>({ Point(-42, 10), Point(3, 4), Point(43, 20) }));
+    shapes.emplace_back(vector<Point>({ Point(0, 0), Point(0, 4), Point(4, 4), Point(4, 0) }), true); // квадрат
+
+    PrintContainer(shapes);
 
 	// b) общее кол-во вершин
 	int totalVertexes = accumulate(shapes.begin(), shapes.end(), 0, [](int sumFar, const Shape& shape) { return sumFar + shape.GetVertexNum(); });
+   
+    cout << endl << "total vertexes: " << totalVertexes << endl;
 
 	// c) кол-во треугольников, квадратов и прямоугольников
-	int squares = count_if(shapes.begin(), shapes.end(), IsSquareFunct());
-	int triangles = count_if(shapes.begin(), shapes.end(), IsTriFunct());
-	int rectangles = count_if(shapes.begin(), shapes.end(), IsRectFunct());
+	int squaresNum = count_if(shapes.begin(), shapes.end(), IsSquareFunct());
+	int trianglesNum = count_if(shapes.begin(), shapes.end(), IsTriFunct());
+	int rectanglesNum = count_if(shapes.begin(), shapes.end(), IsRectFunct());
+
+    cout <<  "triangles: " << trianglesNum << endl << "squares: " << squaresNum << endl << "rectangles: " << rectanglesNum << endl;
 
 	// d) удаление всех пятиугольников
 	shapes.erase(remove_if(shapes.begin(), shapes.end(), [](const Shape& shape) { return shape.GetVertexNum() == 5; }), shapes.end());
+    
+    cout << endl << "[Removing pentagons...]" << endl;
+    PrintContainer(shapes);
 
 	// e) вектор в котором по вершине из каждой фигуры из shapes
 	vector<Point> points(shapes.size());
 	transform(shapes.begin(), shapes.end(), points.begin(), [](const Shape& shape) { return shape.vertexes[rand() % shape.GetVertexNum()]; });
+        
+    cout << endl << "[Points vector]" << endl;
+    PrintContainer(points);
 
 	// f) сортировка: треугольники, квадраты, прямоугольники
-	//partition()
+    auto secondGroup = partition(shapes.begin(), shapes.end(), IsTriFunct());
+    secondGroup = partition(secondGroup, shapes.end(), IsSquareFunct());
+    partition(secondGroup, shapes.end(), IsRectFunct());
+
+    cout << endl << "[Sorting: triangles, squares, rectangles]" << endl;
+    PrintContainer(shapes);
 }
 
 void Task1()
@@ -150,8 +184,11 @@ void Task1()
 
 int main()
 {
+    cout << "[Task 1]" << endl;
 	Task1();
-	Task2();
+	
+    cout << endl << "[Task 2]" << endl;
+    Task2();
 
 	return 0;
 }
